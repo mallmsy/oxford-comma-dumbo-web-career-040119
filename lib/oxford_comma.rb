80bebc04b150f9608bce1bf20884ae6ae4247916
Mallory Woods <mallorywoods@Mallorys-iMac.lan>
2019-03-14 17:41:09 -0400

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join " and "
+  elsif array.length >= 3
+    final_array = array[0..-2].join ', '
+    final_array + ", and #{array.last}"
+  end
 end
